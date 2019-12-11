5352ec5502890a7fba2e791097b0a1eeef01838a
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-12-10 22:59:44 -0300

    first logical for convert message in nunbers phone

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,53 @@ routes.get('/', (req, res) => {
 routes.post('/message-to-numbers', (req, res) => {
   const { message } = req.body;
 
-  return res.json({ api: 'ok' });
+  const parts = message.split('');
+  const dictionary = {
+    A: 2,
+    B: 22,
+    C: 222,
+    D: 3,
+    E: 33,
+    F: 333,
+    G: 4,
+    H: 44,
+    I: 444,
+    J: 5,
+    K: 55,
+    L: 555,
+    M: 6,
+    N: 66,
+    O: 666,
+    P: 7,
+    Q: 77,
+    R: 777,
+    S: 7777,
+    T: 8,
+    U: 88,
+    V: 888,
+    W: 9,
+    X: 99,
+    Y: 999,
+    Z: 9999,
+    ' ': 0,
+  };
+
+  let numbers = '';
+
+  parts.forEach((part, index) => {
+    // console.log(index, part);
+
+    numbers += dictionary[part];
+
+    if (
+      index > 0 &&
+      dictionary[parts[index - 1]] == String(dictionary[part]).substr(1, 1)
+    ) {
+      numbers += '_';
+    }
+  });
+
+  return res.json({ numbers });
 });
 
 export default routes;
